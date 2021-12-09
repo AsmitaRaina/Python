bf6d49bf262a4ad8105f43656449e55a2dc51876
AsmitaRaina <95634515+AsmitaRaina@users.noreply.github.com>
2021-12-09 16:32:18 +0530

    Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,18 @@
+# Assignment 1.0
+""" 
+Q1) 10,1,8,3,6,5,4,7,x,y
+general solution = m(+ or -)n
+where m = the previous number 
+n = the next odd number 
++ if the previous sign is -
+- if the previous sign is + 
+ALGORITHM-
+1) take the difference of the pair of numbers
+2) check whether the second number is greater or less than the first
+3) find the odd number for the next consecutive pair
+4) find whether to add or subtract that odd number"""
+
+#Q2) Check floor division with 3 values
+print(10//2)
+print(9//2)
+print(5//2)
