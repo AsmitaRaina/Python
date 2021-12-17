54e488eff07e9a850273524acc7e1f023bcd3ac8
AsmitaRaina <95634515+AsmitaRaina@users.noreply.github.com>
2021-12-17 23:06:22 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,20 @@
 
-print("1. Addition \
+def calculator(a,b):
+    print("1. Addition \
       2. Subtraction \
     3. Multiplication \
     4. Division")
-n=int(input("Choose your option"))
-a=int(input("Enter first numbre"))
+    n=int(input("Choose your option"))
+    if n==1:
+        print(a+b)
+    elif n==2:
+        print(a-b)
+    elif n==3:
+        print(a*b)
+    elif n==4:
+        print(a/b)
+    
+a=int(input("Enter first number"))
 b=int(input("Enter second number"))
-if n==1:
-    print(a+b)
-elif n==2:
-    print(a-b)
-elif n==3:
-    print(a*b)
-elif n==4:
-    print(a/b)
+calculator(a,b)
+
